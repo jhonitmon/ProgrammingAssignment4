490d90b15730dceda8c3d66f09049033f8a080db
jhonitmon <joanthan.giraldo@hotmail.com>
2022-04-17 23:27:27 +0200

    Final code

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,7 @@
 library(dplyr)
-library(stringr)
 
 features <- read.table("./UCI HAR Dataset/features.txt")[,2];
-
-#names(final_df) <- c("Subject","Activity",features);
 train_df1 <- read.table("./UCI HAR Dataset/train/X_train.txt",col.names = features);
-# colnames(train_df1) <- features;
 train_df2 <- read.table("./UCI HAR Dataset/train/y_train.txt",col.names = c("Activity"));
 train_sub <- read.table("./UCI HAR Dataset/train/subject_train.txt",col.names = c("Subject"));
 train_df3 <- cbind(train_sub,train_df2,train_df1);
@@ -15,9 +11,7 @@ test_df2 <- read.table("./UCI HAR Dataset/test/y_test.txt",col.names = c("Activi
 test_sub <- read.table("./UCI HAR Dataset/test/subject_test.txt",col.names = c("Subject"));
 test_df3 <- cbind(test_sub,test_df2,test_df1);
 test_df3 <- test_df3 %>% select(Subject,Activity,contains("mean"), contains("std"));
-
 final_df <- rbind(train_df3,test_df3);
-# final_df <- final_df[,features];
 final_df <- arrange(final_df,Subject, Activity);
 final_df$Subject <- as.factor(final_df$Subject);
 final_df$Activity <- as.factor(final_df$Activity);
@@ -35,3 +29,5 @@ names(final_df) <- gsub("-mean()",".Mean",names(final_df));
 names(final_df) <- gsub("-std()",".STD",names(final_df));
 names(final_df) <- gsub("-freq()",".Frequency",names(final_df));
 summarized_df <- final_df %>% group_by(Subject,Activity) %>% summarise(across(everything(), mean))
+
+write.table(summarized_df, "tidydata.csv")
